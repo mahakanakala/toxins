70beff6f54ad7d2fa7c78baa4e36c4978385f542
mahakanakala <maha.kanakala@gmail.com>
2023-10-08 02:53:53 -0400

    add quiz logic

--- app/components/recommendation.tsx
+++ app/components/recommendation.tsx
@@ -3,115 +3,175 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import styles from '../page.module.css';
 
+interface SelectedOptions {
+  am: boolean;
+  pm: boolean;
+  oily: boolean;
+  normal: boolean;
+  combination: boolean;
+  dry: boolean;
+  under50: boolean;
+  under70: boolean;
+  under100: boolean;
+}
+
 const SkincareRecommendation: React.FC = () => {
   const [recommendedProduct, setRecommendedProduct] = useState<any>(null);
+  const [selectedOptions, setSelectedOptions] = useState<SelectedOptions>({
+    am: false,
+    pm: false,
+    oily: false,
+    normal: false,
+    combination: false,
+    dry: false,
+    under50: false,
+    under70: false,
+    under100: false,
+  });
 
-  const handleRecommendation = async () => {
-    try {
-      const userData = {
-        skintype: 'oily',
-        routine: 'AM',
-        max_price: 50,
-      };
+  // Function to handle option selection
+  const handleOptionClick = (key: keyof SelectedOptions) => {
+    setSelectedOptions((prevOptions) => ({
+      ...prevOptions,
+      [key]: !prevOptions[key],
+    }));
 
-      const response = await axios.post('/api/recommendation', userData);
-      setRecommendedProduct(response.data);
-    } catch (error) {
-      console.error(error);
-    }
+    
   };
-
 
   return (
     <div className={styles.quizContainer}>
       <div className={styles.questionWrapper}>
-      <div className={styles.question1Img}>
-        <div className={styles.question1}>
-          <h1 className={styles.mainQuestion}>Build a routine! <br/> </h1>
-          <h4 className={styles.quizDescription}>Build a skincare routine with clean products</h4>
-        </div>
-        <div className={styles.question1Options}>
-          <ul className={styles.quizOptions}>
-            <li>
-              <button className={styles.formButton} type="button" title="AM" placeholder='AM' >
-             
-            </button>  
-            <h4 className={styles.quizOptionText}>AM</h4> 
-            </li>
-            <li >
-              <button className={styles.formButton} type="button" title="PM" onClick={() => console.log("PM clicked!")} >
-            </button>
-            <h4 className={styles.quizOptionText}>PM</h4>
-            </li>
-          </ul>
-        </div>
-        </div>
-      </div>
-      <div className={styles.question2Wrapper}>
-      <div className={styles.question2Img}>
-        <div className={styles.question2}>
-          <h1  className={styles.mainQuestion} >Skin Type</h1>
+        <div className={styles.question1Img}>
+          <div className={styles.question1}>
+            <h1 className={styles.mainQuestion}>Build a routine! <br /> </h1>
+            <h4 className={styles.quizDescription}>Build a skincare routine with clean products</h4>
           </div>
-          </div>
-        <div className={styles.question2Options}>
-          <ul className={styles.quizOptions}>
-            <li>
-              <button className={styles.formButton} type="button" title="Oily" >             
-            </button>
-            <h4 className={styles.quizOptionText}>Oily</h4>
-            </li>
-            <li>
-              <button className={styles.formButton} type="button" title="Normal"  >
-            </button>
-            <h4 className={styles.quizOptionText}>Normal</h4>
-            </li>
-            <li>
-              <button className={styles.formButton} type="button" title="Combination" >             
-            </button>
-            <h4>Combination</h4>
-            </li>
-            <li>
-              <button className={styles.formButton} type="button" title="Dry" >            
-            </button>
-            <h4 className={styles.quizOptionText}>Dry</h4>
-            </li>
-          </ul>
-        </div>
-        
-      </div>
-      <div className={styles.question3Wrapper}>
-      <div className={styles.question3Img}>
-        <div className={styles.question3}>
-          <h1  className={styles.mainQuestion}>Price Range</h1>
-          <div className={styles.question3Options}>
+          <div className={styles.question1Options}>
             <ul className={styles.quizOptions}>
               <li>
-                <button className={styles.formButton} type="button" title="Under $50" >
-              </button>
-              <h4 className={styles.quizOptionText}>Under $50</h4>
-              </li>
-              <li>
-                <button className={styles.formButton} type="button" title="Under $70" > 
+                <button
+                  className={styles.formButton}
+                  type="button"
+                  title="AM"
+                  placeholder="AM"
+                  onClick={() => handleOptionClick('am')}
+                >
                 </button>
-                <h4>Under $70</h4>
+                <h4 className={styles.quizOptionText}>AM</h4>
               </li>
-              <li>
-                <button className={styles.formButton} type="button" title="Under $100">
-                  </button>
-                  <h4>Under $100</h4>
+              <li >
+                <button
+                  className={styles.formButton}
+                  type="button"
+                  title="PM"
+                  onClick={() => handleOptionClick("pm")}
+                >
+                </button>
+                <h4 className={styles.quizOptionText}>PM</h4>
               </li>
             </ul>
           </div>
         </div>
+      </div>
+      <div className={styles.question2Wrapper}>
+        <div className={styles.question2Img}>
+          <div className={styles.question2}>
+            <h1 className={styles.mainQuestion} >Skin Type</h1>
+          </div>
         </div>
+        <div className={styles.question2Options}>
+          <ul className={styles.quizOptions}>
+            <li>
+              <button
+                className={styles.formButton}
+                type="button"
+                title="Oily"
+                onClick={()=> handleOptionClick("oily")}
+              >
+              </button>
+              <h4 className={styles.quizOptionText}>Oily</h4>
+            </li>
+            <li>
+              <button
+                className={styles.formButton}
+                type="button"
+                title="Normal"
+                onClick={() => handleOptionClick("normal")}>
+              </button>
+              <h4 className={styles.quizOptionText}>Normal</h4>
+            </li>
+            <li>
+              <button
+                className={styles.formButton}
+                type="button"
+                title="Combination"
+                onClick={() => handleOptionClick("combination")}
+              >
+              </button>
+              <h4>Combination</h4>
+            </li>
+            <li>
+              <button
+                className={styles.formButton}
+                type="button"
+                title="Dry"
+                onClick={() => handleOptionClick("dry")}>
+              </button>
+              <h4 className={styles.quizOptionText}>Dry</h4>
+            </li>
+          </ul>
         </div>
-      <button className={styles.submitBtn} onClick={handleRecommendation}>Submit</button>
+
+      </div>
+      <div className={styles.question3Wrapper}>
+        <div className={styles.question3Img}>
+          <div className={styles.question3}>
+            <h1 className={styles.mainQuestion}>Price Range</h1>
+            <div className={styles.question3Options}>
+              <ul className={styles.quizOptions}>
+                <li>
+                  <button
+                    className={styles.formButton}
+                    type="button"
+                    title="Under $50"
+                    onClick={() => handleOptionClick("under50")}>
+                  </button>
+                  <h4 className={styles.quizOptionText}>Under $50</h4>
+                </li>
+                <li>
+                  <button
+                    className={styles.formButton}
+                    type="button"
+                    title="Under $70"
+                    onClick={() => handleOptionClick("under70")}>
+                  </button>
+                  <h4>Under $70</h4>
+                </li>
+                <li>
+                  <button
+                    className={styles.formButton}
+                    type="button"
+                    title="Under $100"
+                    onClick={() => handleOptionClick("under100")}>
+                  </button>
+                  <h4>Under $100</h4>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button
+        className={styles.submitBtn}
+        // onClick={handleRecommendation}
+      >Submit</button>
       {recommendedProduct && (
         <div className={styles.recommendedProduct}>
           <h2>Recommended Product</h2>
           <p>Name: {recommendedProduct.Name}</p>
           <p>Brand: {recommendedProduct.Brand}</p>
-          
+
         </div>
       )}
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,37 @@
 "use client";
 import styles from "./page.module.css";
 import AnimatedArrow from "./components/AnimatedArrow";
-import Scrolling from "./components/Scrolling"
+import Scrolling from "./components/Scrolling";
 import TypingEffect from "./components/TypingEffect";
 import { BiSearch } from "react-icons/bi";
 import Scanner from './components/scanner';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Html5QrcodeScanner, Html5QrcodeScanType } from 'html5-qrcode';
+
+interface QrCodeConfig {
+  fps: number;
+  qrbox: { width: 250; height: 250 };
+  rememberLastUsedCamera: boolean;
+  supportedScanTypes: Html5QrcodeScanType[];
+}
 
 export default function Home() {
   const typingText = "The Ordinary Hyaluronic Acid";
   const [qrCodeData, setQrCodeData] = useState<string>("");
+  let html5QrcodeScanner: Html5QrcodeScanner;
 
   const handleQrCodeScan = (decodedText: string) => {
     // Handle the QR code scan result here
     setQrCodeData(decodedText);
   };
+
+  const config: QrCodeConfig = {
+    fps: 10,
+    qrbox: { width: 250, height: 250 },
+    rememberLastUsedCamera: true,
+    supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA],
+  };
+
   return (
     <main className={styles.wrapperMain}>
       <div className={styles.landingHeaderBox}>
@@ -24,22 +41,25 @@ export default function Home() {
       <div className={styles.headerBox}>
         <h1 className={styles.searchwebtitle}>Type your product here</h1>
         <div className={styles.safeSearchContainer}>
-          <TypingEffect text={typingText} />
+        <TypingEffect text={typingText} />
+        <div className={styles.searchContainer}>
           <button title="submit" className={styles.safeSubmitBtn} type="submit">
             <BiSearch />
           </button>
-          <div className={styles.qrCodeBox}>
-        <h2>Scanned QR Code Data:</h2>
-        <div className={styles.qrCodeData}>{qrCodeData}</div>
-          </div>
-          <Scanner qrCodeSuccessCallback={handleQrCodeScan} />
         </div>
-        <p className={styles.safeDisclaimer}>This dataset contains a limited number of products available. If the product you're looking for is not available, we apologize.</p>
-        {/* <AnimatedArrow /> */}
+        <Scanner qrCodeSuccessCallback={handleQrCodeScan} />
+      </div>
+        <div className={styles.qrCodeBox}>
+        <h2 className={styles.scanMessage}>Scanned QR Code Data: </h2>
+        <div className={styles.qrCodeData}>{qrCodeData}</div>
+      </div>
+        <p className={styles.safeDisclaimer}>
+          This dataset contains a limited number of products available. If the product you're looking for is not available, we apologize.
+        </p>
       </div>
       <div className={styles.learnMoreLink}>
         <a title='project' href='/about' target="_self" rel="noopener noreferrer">
-        <h2 className={styles.learnMoreText}>Click to</h2>
+          <h2 className={styles.learnMoreText}>Click to</h2>
           <h2 className={styles.learnMoreText2}> Learn more.</h2>
         </a>
       </div>
